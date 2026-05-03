dd2618a6f5142b17dd73fc9fefa1476e988f88e7
Josue Reyes <110793591+JRP06@users.noreply.github.com>
2026-05-02 19:01:16 -0500

    initial project setup with bob_sessions folder

---
+++ src/main/java/com/truckadvisor/dieselfaultadvisor/DieselfaultadvisorApplication.java
@@ -0,0 +1,13 @@
+package com.truckadvisor.dieselfaultadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieselfaultadvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieselfaultadvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truckadvisor/dieselfaultadvisor/DieselfaultadvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truckadvisor.dieselfaultadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieselfaultadvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
